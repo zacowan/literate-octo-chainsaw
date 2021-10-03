38cf52285600a19d0bde0b5d6db89bcb2788603b
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-10-03 14:28:20 -0400

    feat(peerProcess): coded during meeting
    Co-authored-by: sahir1359 <sahir1359@users.noreply.github.com>
    Co-authored-by: John,Joel A <joeljohn@ufl.edu>

--- src/p2p/Server.java
+++ src/p2p/Server.java
@@ -8,11 +8,9 @@ import java.util.*;
 
 public class Server {
 
-	private static final int sPort = 8000; // The server will be listening on this port number
-
-	public static void main(String[] args) throws Exception {
+	public void run(int port) throws Exception {
 		System.out.println("The server is running.");
-		ServerSocket listener = new ServerSocket(sPort);
+		ServerSocket listener = new ServerSocket(port);
 		int clientNum = 1;
 		try {
 			while (true) {
---
+++ src/p2p/peerProcess.java
@@ -0,0 +1,90 @@
+package p2p;
+
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+import java.net.Socket;
+
+public class peerProcess {
+
+  public static void main(String args[]) {
+    // args[0] = peerID
+    String peerID = args[0];
+
+    // TODO: use me
+    String numberOfPreferredNeighbors;
+    String unchokingInterval;
+    String optimisticUnchokingInterval;
+    String fileName;
+    String fileSize;
+    String pieceSize;
+
+    // read Common.cfg
+    File commonFile = new File("Common.cfg");
+    try {
+      Scanner scanner = new Scanner(commonFile);
+      numberOfPreferredNeighbors = scanner.nextLine().split(" ")[1];
+      unchokingInterval = scanner.nextLine().split(" ")[1];
+      optimisticUnchokingInterval = scanner.nextLine().split(" ")[1];
+      fileName = scanner.nextLine().split(" ")[1];
+      fileSize = scanner.nextLine().split(" ")[1];
+      pieceSize = scanner.nextLine().split(" ")[1];
+      scanner.close();
+    } catch (Exception e) {
+      System.out.printf("Error reading Common.cfg: %s", e.getMessage());
+    }
+
+    ArrayList<PeerInfo> peerList = new ArrayList<PeerInfo>();
+    PeerInfo thisPeer = null;
+
+    // read PeerInfo.cfg
+    File peerInfoFile = new File("PeerInfo.cfg");
+    try {
+      Scanner scanner = new Scanner(peerInfoFile);
+      while (scanner.hasNextLine()) {
+        String[] line = scanner.nextLine().split(" ");
+        PeerInfo currentPeer = new PeerInfo(line[0], line[1], line[2], line[3]);
+        peerList.add(currentPeer);
+        if (currentPeer.peerID == peerID) {
+          thisPeer = currentPeer;
+        }
+      }
+      scanner.close();
+    } catch (Exception e) {
+      System.out.printf("Error reading PeerInfo.cfg: %s", e.getMessage());
+    }
+
+    // spawn server
+    try {
+      if (thisPeer != null) {
+        // TODO: Joel
+        // server should have access to peerList
+        // server should run in its own thread
+        new Server().run(Integer.parseInt(thisPeer.port));
+        // new Thread(new Send(server)).start();
+        // new Thread(new Recieve(server)).start();
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    // spawn X clients
+    PeerInfo currentPeer = peerList.get(0);
+    while (currentPeer != thisPeer) {
+      // TODO: Zach
+      // each client should be uniquely identifyable
+      // each client should have access to peerList
+      // each client should run in its own thread
+      new Client().run();
+    }
+
+    // peerList of all peers
+    // while (peers.notHaveFile) wait
+    // when (peers.haveFile) terminate
+  }
+
+  // set parameters
+  // start server
+  // connect clients to other peers if there
+}
