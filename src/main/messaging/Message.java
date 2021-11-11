20840d3ed1ca71bf15da438414c2211259466e02
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2021-11-11 17:59:32 -0500

    fix: fixed sources

--- src/main/messaging/Message.java
+++ src/main/messaging/Message.java
@@ -7,6 +7,7 @@ import java.nio.channels.*;
 import java.util.*;
 
 import main.logging.*;
+import main.messaging.payloads.BitfieldPayload;
 import main.messaging.payloads.EmptyPayload;
 import main.messaging.payloads.Payload;
 import main.messaging.payloads.PiecePayload;
@@ -87,6 +88,8 @@ public class Message {
       return new RequestPayload(payload);
     case PIECE:
       return new PiecePayload(payload);
+    case BITFIELD:
+      return new BitfieldPayload(payload);
     default:
       return new EmptyPayload();
     }
